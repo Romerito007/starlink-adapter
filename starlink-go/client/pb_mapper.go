7c6a199996f6481671938c426dc03d50aa0d7d09
Romerito Alexandre <romerito_alexandre007@hotmail.com>
2026-03-26 20:05:49 -0300

    Rewrite starlink-go README for production network topology guidance

--- starlink-go/client/pb_mapper.go
+++ starlink-go/client/pb_mapper.go
@@ -14,7 +14,7 @@ func mapStatus(in *pb.DishGetStatusResponse) *Status {
 		DeviceID:              deviceInfo.GetId(),
 		HardwareVersion:       deviceInfo.GetHardwareVersion(),
 		SoftwareVersion:       deviceInfo.GetSoftwareVersion(),
-		State:                 deviceState.GetUptimeState().String(),
+		UptimeSeconds:         deviceState.GetUptimeS(),
 		UplinkThroughputBps:   in.GetUplinkThroughputBps(),
 		DownlinkThroughputBps: in.GetDownlinkThroughputBps(),
 		PopPingDropRate:       in.GetPopPingDropRate(),
@@ -42,6 +42,13 @@ func mapLocation(in *pb.GetLocationResponse) *Location {
 	}
 
 	lla := in.GetLla()
+	if lla == nil {
+		return &Location{
+			SigmaM: in.GetSigmaM(),
+			Source: in.GetSource().String(),
+		}
+	}
+
 	return &Location{
 		Latitude:  lla.GetLat(),
 		Longitude: lla.GetLon(),
