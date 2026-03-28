12c37064502a30462ebdbddc2b1b4b992a78b9a2
Romerito Alexandre <romerito_alexandre007@hotmail.com>
2026-03-28 11:41:33 -0300

    Add GetDhcpLeases API using wifi status DHCP servers

--- starlink-go/client/models.go
+++ starlink-go/client/models.go
@@ -32,26 +32,61 @@ type Location struct {
 
 // ClientDevice is the normalized domain model for a connected LAN/Wi-Fi client.
 type ClientDevice struct {
-	MacAddress            string
-	IpAddress             string
-	Interface             string
-	InterfaceName         string
-	Role                  string
-	SignalStrength        float32
-	Snr                   float32
-	ChannelWidth          uint32
-	Mode                  string
-	Blocked               bool
-	DhcpLeaseActive       bool
-	DhcpLeaseRenewed      bool
-	AssociatedTimeSeconds uint32
-	NoDataIdleSeconds     uint32
-	RxRateMbps            uint32
-	TxRateMbps            uint32
-	RxRateMbpsLast15s     float32
-	TxRateMbpsLast15s     float32
-	Name                  string
-	GivenName             string
-	Domain                string
-	Ipv6Addresses         []string
+	MacAddress                   string
+	IpAddress                    string
+	Interface                    string
+	InterfaceName                string
+	UpstreamMacAddress           string
+	Role                         string
+	SignalStrength               float32
+	Snr                          float32
+	ChannelWidth                 uint32
+	Mode                         string
+	Blocked                      bool
+	DhcpLeaseActive              bool
+	DhcpLeaseRenewed             bool
+	DhcpLeaseFound               bool
+	SecondsUntilDhcpLeaseExpires uint32
+	AssociatedTimeSeconds        uint32
+	NoDataIdleSeconds            uint32
+	HopsFromController           uint32
+	ClientID                     uint32
+	CaptiveClientID              uint32
+	UploadMb                     float32
+	DownloadMb                   float32
+	RxStatsValid                 bool
+	TxStatsValid                 bool
+	RxBytes                      uint64
+	TxBytes                      uint64
+	RxNss                        int32
+	TxNss                        int32
+	RxMcs                        uint32
+	TxMcs                        uint32
+	RxBandwidth                  uint32
+	TxBandwidth                  uint32
+	RxGuardNs                    uint32
+	TxGuardNs                    uint32
+	RxRateMbps                   uint32
+	TxRateMbps                   uint32
+	RxPhyMode                    uint32
+	TxPhyMode                    uint32
+	RxRateMbpsLast15s            float32
+	TxRateMbpsLast15s            float32
+	RxRateMbpsLast1mAvg          float32
+	TxRateMbpsLast30s            float32
+	Name                         string
+	GivenName                    string
+	Domain                       string
+	Ipv6Addresses                []string
+}
+
+// DhcpLease is the normalized domain model for a DHCP lease entry.
+type DhcpLease struct {
+	IpAddress   string
+	MacAddress  string
+	Hostname    string
+	ExpiresTime string
+	Active      bool
+	ClientID    uint32
+	Domain      string
 }
